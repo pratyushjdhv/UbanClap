225e69a1dd2d5593f85f6f145275c5e941a1e0da
pratyush <pratyushjdhv@gmail.com>
2024-11-30 07:55:37 +0000

    refactor(Navbar): streamline logout method and improve formatting refactor(create_service): adjust formatting for consistency

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,16 +1,20 @@
 export default {
-    template : `
+    template: `
     <div>
         <router-link to='/'>Home</router-link>
-        <router-link  v-if="!$store.state.loggedIn" to='/login'>Login</router-link>
-        <router-link  v-if="!$store.state.loggedIn" to='/register'>Register</router-link>
+        <router-link v-if="!$store.state.loggedIn" to='/login'>Login</router-link>
+        <router-link v-if="!$store.state.loggedIn" to='/register'>Register</router-link>
 
         <router-link v-if="$store.state.loggedIn && $store.state.role == 'admin'" to='/admin-dashboard'>Admin Dash</router-link>
         <router-link v-if="$store.state.loggedIn" to='/feed'>Feed</router-link>
 
-
-        <button class="btn btn-secondary" v-if="$store.state.loggedIn" @click="$store.commit('logout')">Logout</button>
-
+        <button class="btn btn-secondary" v-if="$store.state.loggedIn" @click="logout">Logout</button>
     </div>
-    `
+    `,
+    methods: {
+        logout() {
+            this.$store.commit('logout');
+            this.$router.push('/login');
+        }
+    }
 }
